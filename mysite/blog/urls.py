be6711ed23fb13ad557d7893f0d8469d3709beca
antmont <antmontalt@me.com>
2017-08-13 22:07:50 +1000

    To Django Girls end of Django Forms

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     # /blog/post/5/
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    # /blog/post/new/
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    # /blog/post/5/edit/
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
