3c4a0a784025875123b487e7d3bf88d5e7148c8c
antmont <antmontalt@me.com>
2017-08-13 12:36:08 +1000

    Django girls to end of extend your application

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     # /blog/
     url(r'^$', views.post_list, name='post_list'),
+    # /blog/post/5/
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
