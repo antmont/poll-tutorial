cb4b0f72a9ff4eadb938036407f893aac7487dbe
antmont <antmontalt@me.com>
2017-08-17 18:46:33 +1000

    Github user data tutorial added

---
+++ mysite/ghud/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GhudConfig(AppConfig):
+    name = 'ghud'
+    verbose_name = "github user data"
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^blog/', include('blog.urls')),
+    url(r'^ghud/', include('ghud.urls')),
     url(r'^admin/', admin.site.urls),
 ]
